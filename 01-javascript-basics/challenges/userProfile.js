3ba37a8fdb1948ddacdfa8273320cfaa17acf963
shadali <shadalimalek@gmail.com>
2025-07-18 20:11:42 +0530

    Add user profile object and console log for user information

---
+++ 01-javascript-basics/challenges/userProfile.js
@@ -0,0 +1,8 @@
+const user = {
+    username: "Shad",
+    age: 19,
+    learning: "Backend Development",
+    isMotivated: true
+};
+
+console.log(`Hi, I'm ${user.username} (${user.age}). Currently learning ${user.learning}. Motivated? ${user.isMotivated}`);
